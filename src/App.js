0daf4e2325aa55f39a93e865124cb994c43c3ea5
Jody Kirton <jody2be@yahoo.ca>
2020-12-11 23:28:51 -0500

    update menu-tiems to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
-  return <div className="App">
-    <HomePage />
-  </div>;
+  return (
+  <div className="App">
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+      <Route path='/jackets' component={HatsPage} />
+      <Route path='/sneakers' component={HatsPage} />
+      <Route path='/womens' component={HatsPage} />
+      <Route path='/mens' component={HatsPage} />
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
