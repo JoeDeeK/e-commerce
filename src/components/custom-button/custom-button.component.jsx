ad470995542db717c739c915bdc6acf90d43a5c2
Jody Kirton <jody2be@yahoo.ca>
2020-12-14 16:29:18 -0500

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,12 @@
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
     <button
-        className='custom-button' {...otherProps}>{children}</button>
+        className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} 
+        {...otherProps}
+    >
+        {children}
+    </button>
 );
 
 export default CustomButton;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBzmcpMNWRTaJBAj00bFQZ1BD_2i8OIyHs",
+    authDomain: "e-commerce-eb426.firebaseapp.com",
+    projectId: "e-commerce-eb426",
+    storageBucket: "e-commerce-eb426.appspot.com",
+    messagingSenderId: "226863318124",
+    appId: "1:226863318124:web:fa882599566a1bd5bcc449",
+    measurementId: "G-7SFDQRSVLM"
+};
+
+// if need to load multiple apps, check app names
+if (!firebase.apps.length) { firebase.initializeApp(config); }
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
